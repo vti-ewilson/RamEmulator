5ecfd5da039ae17ce5e07a99e5f7bcb047efa2c1
Eli Wilson <ewilson@vacuumtechnology.com>
2025-04-01 12:48:11 -0400

    add files

---
+++ RamEmulatorForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RamEmulator
+{
+	public partial class RamEmulatorForm : Form
+	{
+		SerialPort port;
+		System.Threading.Thread comThread;
+		bool disconnectClicked = false;
+		int counts = 0;
+
+		public RamEmulatorForm()
+		{
+			InitializeComponent();
+		}
+
+		private void populateComPortMenu()
+		{
+			COMPortDropdown.Items.AddRange(
+					System.IO.Ports.SerialPort.GetPortNames()
+						.OrderBy(s => s)
+						.Distinct()
+						.ToArray());
+		}
+
+		private void RamEmulatorForm_Load(object sender, EventArgs e)
+		{
+			populateComPortMenu();
+			timer1.Enabled = true;
+		}
+
+		private string GetDropdownValue()
+		{
+			if(COMPortDropdown.SelectedItem != null)
+				return COMPortDropdown.SelectedItem.ToString();
+			else
+				return "";
+		}
+
+		private void SetButtonStates(bool connected)
+		{
+			if(connected)
+			{
+				connectButton.Enabled = false;
+				disconnectButton.Enabled = true;
+				connectButton.BackColor = Color.LightGreen;
+				disconnectButton.BackColor = Color.White;
+			}
+			else
+			{
+				connectButton.Enabled = true;
+				disconnectButton.Enabled = false;
+				disconnectButton.BackColor = Color.Red;
+				connectButton.BackColor = Color.White;
+			}
+
+		}
+
+		private void communicate()
+		{
+			string portName = (string)COMPortDropdown.Invoke(new Func<string>(() => GetDropdownValue()));
+			if(portName == "")
+			{
+				connectButton.Invoke(new Action(() => SetButtonStates(false)));
+				return;
+			}
+			port = new SerialPort(portName, 9600);
+			port.Open();
+			byte[] buffer;
+			string recd, msg;
+
+			Random rand = new Random();
+
+			// Generate random values for each byte
+			byte statusByte = 0xf0;
+			byte alarms = 0xff;
+			byte inputs = 0xff;
+			byte outputs = 0xff;
+
+			// Construct response string (ensuring proper hex format with leading zeros)
+			string response = "RESP" +
+				statusByte.ToString("X2") +
+				alarms.ToString("X2") +
+				inputs.ToString("X2") +
+				outputs.ToString("X2") +
+				outputs.ToString("X2") +
+				outputs.ToString("X2") +
+				outputs.ToString("X2") +
+				outputs.ToString("X2");
+
+			while(!disconnectClicked)
+			{
+				int count = port.BytesToRead;
+				if(count >= 2)
+				{
+					buffer = new byte[count];
+					var str = port.Read(buffer, 0, count);
+					recd = Encoding.Default.GetString(buffer);
+					Console.WriteLine(recd);
+					
+					port.WriteLine(response);
+					Console.WriteLine(response);
+				}
+				else
+				{
+					Thread.Sleep(50);
+				}
+			}
+			disconnectClicked = false;
+			port.Close();
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			countLabel.Text = counts.ToString() + " counts";
+		}
+
+		private void connectButton_Click(object sender, EventArgs e)
+		{
+			SetButtonStates(true);
+
+			comThread = new Thread(() => communicate());
+			comThread.Start();
+		}
+
+		private void disconnectButton_Click(object sender, EventArgs e)
+		{
+			SetButtonStates(false);
+			disconnectClicked = true;
+		}
+	}
+}
